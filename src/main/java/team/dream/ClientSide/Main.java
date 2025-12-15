856fd73820b6d1375ab2e302a2ab3993225f0dad
ivbe0111 <berezkinivan94@gmail.com>
2025-12-15 15:46:02 +0100

    added offline option to Main

--- src/main/java/team/dream/ClientSide/Main.java
+++ src/main/java/team/dream/ClientSide/Main.java
@@ -1,8 +1,18 @@
 package team.dream.ClientSide;
 
+import javax.swing.*;
+
 public class Main {
     static void main() {
-       ClientConnection client = ClientConnection.getClientConnection();
-       client.start();
+
+
+        String inputUser = JOptionPane.showInputDialog("Connect to server = 1, Offline = 2");
+        if(inputUser.equals("1")){
+            ClientConnection client = ClientConnection.getClientConnection();
+            client.start();
+        }else{
+            //Do offline stuff
+        }
+
         }
 }
