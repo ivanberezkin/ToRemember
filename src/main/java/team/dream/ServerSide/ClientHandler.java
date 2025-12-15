f7899191e505079f893e91920da888b5d4569560
ivbe0111 <berezkinivan94@gmail.com>
2025-12-15 14:12:11 +0100

    added shell to ClientHandler

--- src/main/java/team/dream/ServerSide/ClientHandler.java
+++ src/main/java/team/dream/ServerSide/ClientHandler.java
@@ -1,4 +1,41 @@
 package team.dream.ServerSide;
 
-public class ClientHandler {
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ClientHandler extends Thread {
+
+    ObjectInputStream inputStream;
+    ObjectOutputStream outputStream;
+
+
+    ClientHandler(Socket socket) {
+        try {
+            this.outputStream = new ObjectOutputStream(socket.getOutputStream());
+            this.inputStream = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void run(){
+        try{
+            while(true){
+                Object inputFromClient = null;
+                inputFromClient = inputStream.readObject();
+
+                if(inputFromClient != null){
+                    //TODO lägga till logik som kollar vad det är för objekt.
+
+                }
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
