9096308b4678242a7414e4ce49dd809436293c76
Niklas Högblom <nhogblom@gmail.com>
2025-12-16 12:00:25 +0100

    SingleMemoryListDatabase.java base structure & methods.

--- src/main/java/team/dream/ServerSide/SingleMemoryListDatabase.java
+++ src/main/java/team/dream/ServerSide/SingleMemoryListDatabase.java
@@ -1,5 +1,40 @@
 package team.dream.ServerSide;
 
-public class SingleMemoryListDatabase {
+import team.dream.shared.MemoryList;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingleMemoryListDatabase {
+    private static final SingleMemoryListDatabase instance = new SingleMemoryListDatabase();
+    private List<MemoryList> memoryLists = new ArrayList<>();
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final String filename = "src/main/resources/MemoryLists.json";
+
+    private SingleMemoryListDatabase() {
+    }
+
+    public static SingleMemoryListDatabase getInstance() {
+        return instance;
+    }
+
+    private List<MemoryList> readMemoryListsFromFileJson() {
+        return mapper.readValue(new File(filename), new TypeReference<List<MemoryList>>() {});
+    }
+
+    private void writeMemoryListsToFile(List<MemoryList> memoryLists) {
+        mapper.writerWithDefaultPrettyPrinter().writeValue(new File(filename), memoryLists);
+    }
+
+
+    public List<MemoryList> getMemoryLists() {
+        return memoryLists;
+    }
+
+    public void setMemoryLists(List<MemoryList> memoryLists) {
+        this.memoryLists = memoryLists;
+    }
 }
