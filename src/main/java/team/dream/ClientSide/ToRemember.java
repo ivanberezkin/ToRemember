e8cd636a96af57302caa88b0825e76e27ec9c17a
ivbe0111 <berezkinivan94@gmail.com>
2025-12-27 10:52:27 +0100

    remove online/offline option in toRemember

--- src/main/java/team/dream/ClientSide/ToRemember.java
+++ src/main/java/team/dream/ClientSide/ToRemember.java
@@ -7,21 +7,15 @@ public class ToRemember {
     static void main() {
 
         try (Scanner sc = new Scanner(System.in)) {
-            IO.println("Connect to server = 1, Offline = 2");
-            String inputUser = sc.nextLine();
-            if(inputUser.equals("1")){
-                ClientConnection client = ClientConnection.getClientConnection();
-                IO.println("Enter username:");
-                client.setUsername(sc.nextLine());
-                client.start();
+            ClientConnection client = ClientConnection.getClientConnection();
+            IO.println("Enter username:");
+            client.setUsername(sc.nextLine());
+            client.start();
 
-                try{
-                    client.join();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }else{
-                //Do offline stuff
+            try {
+                client.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     }
