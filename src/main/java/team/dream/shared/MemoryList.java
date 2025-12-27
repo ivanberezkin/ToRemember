41aa9c6aa48fc588dfa5cf6fad1d9e6150059a55
Viktor <vikvil91@gmail.com>
2025-12-27 10:34:33 +0100

    add param const and methods

--- src/main/java/team/dream/shared/MemoryList.java
+++ src/main/java/team/dream/shared/MemoryList.java
@@ -6,12 +6,34 @@ import java.util.List;
 public class MemoryList {
     private List<Note> notes = new ArrayList<>();
     private String title;
+    private String userID;
+    private List<User> users = new ArrayList<>();
 
-    public MemoryList(List<Note> notes) {
-        this.notes = notes;
+    public MemoryList() {}
+
+    public MemoryList(String title, String userID) {
+        this.title = title;
+        this.userID = userID;
     }
 
-    public MemoryList() {
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void addNote(Note note) {
+        notes.add(note);
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
     }
 
     public List<Note> getNotes() {
