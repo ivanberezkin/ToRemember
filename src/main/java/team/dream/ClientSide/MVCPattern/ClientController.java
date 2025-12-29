e1e8ba04d6f02c68cba20b62109b9a7c040aeaad
ivbe0111 <berezkinivan94@gmail.com>
2025-12-29 19:37:04 +0100

    added two helperMethods classes to make ClientController class a bit more easy to follow

--- src/main/java/team/dream/ClientSide/MVCPattern/ClientController.java
+++ src/main/java/team/dream/ClientSide/MVCPattern/ClientController.java
@@ -1,5 +1,6 @@
 package team.dream.ClientSide.MVCPattern;
 
+import lombok.Data;
 import team.dream.shared.*;
 
 import java.util.ArrayList;
@@ -7,6 +8,7 @@ import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
+@Data
 public class ClientController {
     private ClientModel model;
     private View view;
@@ -18,118 +20,13 @@ public class ClientController {
         this.view = view;
     }
 
-
-    public MemoryList getInputFromShowMemoryLists() {
-        view.showAllMemoryListsView(model.getUsersMemoryList(), model.getSharedMemoryList());
-
-        int inputFromUser = scan.nextInt();
-        scan.nextLine();
-
-        bothOwnedAndSharedList.clear();
-        bothOwnedAndSharedList.addAll(model.getUsersMemoryList());
-        bothOwnedAndSharedList.addAll(model.getSharedMemoryList());
-
-        return bothOwnedAndSharedList.get(inputFromUser - 1);
-
-    }
-
-    private String getTitleForNewMemoryList() {
-        IO.println("Please enter name for your memorylist.");
-        String title = scan.nextLine();
-        //TODO lägga till felhantering
-        return title;
-    }
-
-    public Message getInputFromChosenMemoryList(MemoryList memoryListToShow) {
-        view.showMemoryListView(memoryListToShow);
-        view.showUserOptionForChosenMemoryListView();
-
-        int userChosenOption = scan.nextInt();
-        scan.nextLine();
-        return getInputFromChosenMemoryListAllOptions(userChosenOption, memoryListToShow);
-
-    }
-
-    private Note createNewNote() {
-        IO.println("Creating new note, please enter title: ");
-        String title = scan.nextLine();
-
-        IO.println("Enter description of the note: ");
-        String description = scan.nextLine();
-
-        IO.println("Set priority index (1-5, 1 is highest priority, 5 is lowest): ");
-        int priority = scan.nextInt();
-        scan.nextLine();
-
-        view.categoryEnumPrint();
-        String category = scan.nextLine();
-        Category chosenCategory = Category.valueOf(category.trim().toUpperCase());
-
-        return new Note(title, description, priority, chosenCategory);
-    }
-
-    private Message getChosenNoteForUser(MemoryList chosedMemoryList) {
-        view.showMemoryListView(chosedMemoryList);
-        IO.println("Which note would you like to see?" +
-                "\nEnter 0 to go back " +
-                "\nEnter a valid index");
-        int chosenNoteIndex = scan.nextInt();
-        scan.nextLine();
-        if (chosenNoteIndex == 0) {
-            return new Message(MessageType.SHOW_LIST_OF_MEMORY_LISTS, model.getUser());
-        } else {
-            Note chosenNote = chosedMemoryList.getNotes().get(chosenNoteIndex - 1);
-            chosenNote.printNote();
-            return wouldUserWantToEditNote(chosenNote, chosedMemoryList);
-        }
-
-    }
-
-    private Message wouldUserWantToEditNote(Note note, MemoryList chosedMemoryList) {
-        IO.println("If you want to edit note, enter Title/Description/Priority/Category else enter random character to go back");
-        String userEditInput = scan.nextLine();
-        String userEdit = null;
-        int userEditPriority;
-        switch (userEditInput.trim().toLowerCase()) {
-            case "title" -> {
-                IO.println("Current title: " + note.getTitle());
-                IO.println("Change title to: ");
-                userEdit = scan.nextLine();
-                note.setTitle(userEdit);
-            }
-            case "description" -> {
-                IO.println("Current description: " + note.getDescription());
-                IO.println("Change description to: ");
-                userEdit = scan.nextLine();
-                note.setDescription(userEdit);
-            }
-            case "priority" -> {
-                IO.println("Current priority: " + note.getPriorityIndex());
-                IO.println("Change priority to (1-5 where 1 is highest priority and 5 is lowest): ");
-                userEditPriority = scan.nextInt();
-                scan.nextLine();
-                note.setPriorityIndex(userEditPriority);
-            }
-            case "category" -> {
-                IO.println("Current Category: " + note.getCategoryEnum().toString());
-                view.categoryEnumPrint();
-                userEdit = scan.nextLine();
-                note.setCategoryEnum(Category.valueOf(userEdit.trim().toUpperCase()));
-            }
-            default -> {
-                return new Message(MessageType.SHOW_CHOSEN_MEMORY_LIST, chosedMemoryList, model.getUser());
-            }
-        }
-        return new Message(MessageType.UPDATE_NOTE, chosedMemoryList, model.getUser());
-    }
-
     private Message getInputFromChosenMemoryListAllOptions(int userChosenOption, MemoryList chosedMemoryList) {
         switch (userChosenOption) {
             case 1 -> {
-                return getChosenNoteForUser(chosedMemoryList);
+                return NoteHelperMethods.getChosenNoteForUser(chosedMemoryList, scan, this);
             }
             case 2 -> {
-                chosedMemoryList.addNoteToMemoryList(createNewNote());
+                chosedMemoryList.addNoteToMemoryList(NoteHelperMethods.createNewNote(scan, this));
                 return new Message(MessageType.CREATE_NOTE, chosedMemoryList, model.getUser());
             }
             case 3 -> {
@@ -163,7 +60,7 @@ public class ClientController {
                     }
                     //Create new memoryList
                     case 2 -> {
-                        String title = getTitleForNewMemoryList();
+                        String title = MemoryListHelperMethods.getTitleForNewMemoryList(scan);
                         return new Message(MessageType.CREATE_MEMORY_LIST, title, model.getUser());
                     }
                     case 3 -> {
@@ -181,11 +78,31 @@ public class ClientController {
                 scan.next();
                 IO.println("Please enter a valid digit");
             }
-
-
         }
+    }
 
+    public Message getInputFromChosenMemoryList(MemoryList memoryListToShow) {
+        view.showMemoryListView(memoryListToShow);
+        view.showUserOptionForChosenMemoryListView();
+
+        int userChosenOption = scan.nextInt();
+        scan.nextLine();
+        return getInputFromChosenMemoryListAllOptions(userChosenOption, memoryListToShow);
+    }
+
+    public MemoryList getInputFromShowMemoryLists() {
+        view.showAllMemoryListsView(model.getUsersMemoryList(), model.getSharedMemoryList());
+
+        int inputFromUser = scan.nextInt();
+        scan.nextLine();
+
+        bothOwnedAndSharedList.clear();
+        bothOwnedAndSharedList.addAll(model.getUsersMemoryList());
+        bothOwnedAndSharedList.addAll(model.getSharedMemoryList());
+
+        return bothOwnedAndSharedList.get(inputFromUser - 1);
 
     }
+
 
 }
